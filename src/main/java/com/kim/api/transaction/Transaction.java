157025954ccd4fd66a4b17cecdb034a35ab45bce
doublemetal <lehdcjfl@naver.com>
2020-04-26 11:53:14 +0900

    #2 결제 API 구현 > 결제 정보 모델링

--- src/main/java/com/kim/api/transaction/Transaction.java
+++ src/main/java/com/kim/api/transaction/Transaction.java
@@ -5,6 +5,9 @@ import com.kim.api.transaction.enums.TransactionType;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
@@ -12,7 +15,30 @@ import java.math.BigDecimal;
  */
 @Getter
 @Setter
+@Entity
 public class Transaction {
+    @Id
+    @Column(length = 20)
+    private String transactionId; // 거래시간 + Sequence
+    @Column(length = 450)
+    private String rawData; // 카드사에 전송하는 string 데이터
+    @Column(length = 20)
+    private String originalTransactionId; // 취소의 원거래번호
+    @Column(length = 10)
+    private TransactionType transactionType; // 거래유형
+
+    @Column(length = 20)
+    private String cardNumber; // 카드번호, 10 ~ 20
+    @Column(length = 300)
+    private String encryptedCardNumber; // 암호화한 카드번호, 300자
+
+    @Column(length = 4)
+    private String period; // 유효기간(4자리 숫자, mmyy)
+    private int month; // 할부개월수, 0(일시불), 1 ~ 12, 취소는 일시불(00)으로 저장
+    private int cvc; // cvc(3자리 숫자)
+
+    private BigDecimal payAmount; // 거래금액(100원 이상, 10억원 이하, 숫자), 취소는 결제 금액보다 작아야함
+    private BigDecimal vat; // 부가가치세, 거래금액보다 작아야함, 취소는 원거래와 취소의 부가가치세가 같아야함
 
     /**
      * 결제 Request
@@ -24,7 +50,8 @@ public class Transaction {
         private String period; // 유효기간(4자리 숫자, mmyy)
         private int cvc; // cvc(3자리 숫자)
         private int month; // 할부개월수, 0(일시불), 1 ~ 12
-        private BigDecimal payAmount; // 결제금액(100원 이상, 10억원 이하, 숫자)
+        private BigDecimal payAmount; // 거래금액(100원 이상, 10억원 이하, 숫자)
+        private BigDecimal vat; // 부가가치세
     }
 
     /**
